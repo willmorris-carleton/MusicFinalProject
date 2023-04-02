e7912261ff4004570fec5a7af4a5c20b82cc4bb5
William Morris <56230234+willmorris-carleton@users.noreply.github.com>
2023-04-02 01:28:25 -0400

    Progress

---
+++ Assets/IcicleController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleController : MonoBehaviour
+{
+    [Range(1,5)]
+    public int noteIndex = 1;
+}
--- Assets/WinterMusicController.cs
+++ Assets/WinterMusicController.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class WinterMusicController : MonoBehaviour
 {
+    [SerializeField]
+    LayerMask icicleLayer;
+    
     BoxCollider2D boxCollider2D;
     EventInstance song;
     void Start() {
@@ -20,5 +23,27 @@ public class WinterMusicController : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D col) {
         song.stop(STOP_MODE.ALLOWFADEOUT);
+    }
+
+    void Update() {
+
+        if (Input.GetMouseButton(0)) {
+            song.setParameterByName("PlayingNote", 1);
+        }
+        else {
+            song.setParameterByName("PlayingNote", 0);
+        }
+        
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.up, 0.1f, icicleLayer);
+        if (hit.collider) {
+            IcicleController icicle = hit.collider.GetComponent<IcicleController>();
+            if (icicle) {
+                Debug.Log(icicle.noteIndex);
+                song.setParameterByName("Note", icicle.noteIndex);
+                return;
+            }
+        }
+        song.setParameterByName("Note", 0);
     }
 }
